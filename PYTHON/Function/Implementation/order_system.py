4b56610ec056cbaed814d0de83ab19d0ea31cd74
kanghanbin <hanbin0723@g.yju.ac.kr>
2026-08-25 11:08:49 +0900

    refactor: 주문 시스템 기본 로직 개선

--- PYTHON/Function/Implementation/order_system.py
+++ PYTHON/Function/Implementation/order_system.py
@@ -1,8 +1,8 @@
 # 카페의 주문 시스템 : Cafe order system
 
 # TODO 
-# 0단계 - 메뉴 딕셔너리와 함수의 구조 설정
-# 1단계 — 입력 검증 : 잘못된 메뉴나 옵션을 입력했을 때 프로그램이 터지지 않도록 만들기.
+# 0단계 - 메뉴 딕셔너리와 함수의 구조 설정 : 26-08-24
+# 1단계 — 입력 검증 : 잘못된 메뉴나 옵션을 입력했을 때 프로그램이 터지지 않도록 만들기. : 26-08-25
 # 2단계 — 주문 목록 만들기 : 한 번 주문하고 끝나는 게 아니라 여러 주문을 저장
 # 3단계 — 총 주문 금액 계산 : 주문 목록을 받아서 총액을 계산하는 함수를 추가.
 # 4단계 — 주문 삭제 / 수정 : (주문한 뒤) 주문 수량 변경, 주문 삭제
@@ -45,7 +45,7 @@ menu = {
     "Ade": {
         "Lemonade": 4000,
         "Grapefruit Ade": 5500
-    }
+    }, 
 }
 
 # 주문시킬 메뉴
@@ -66,37 +66,23 @@ def order_menu(menu_name : str, quantity : int):
         => Lemonade - 4,000 * 2 = 8,000
     '''
 
-    # 딕셔너리가 아닐 때 : Cafe Latte
-    if type(menu[menu_name]) != dict:
-        return f"주문한 메뉴: {menu_name}, 가격: {menu[menu_name] * quantity}원"
+    # 딕셔너리인지 아닌지로 구분하는것이 아닌, 메뉴명을 넣었을때 int인지 아닌지를 봐야한다
+    # .get(key : 키 값, default : 반환 값)
+    current = menu.get(menu_name, None)
+    
+    # 무한 반복문 사용 : 일반화, current의 값을 추가하면서 사용
+    while not isinstance(current, int):
+        # current의 값이 menu에 존재하지 않을 때
+        if current not in menu:
+            return "존재하지 않는 메뉴입니다"
+        # current의 값이 menu에 존재할 때
+        for m in menu.keys():
+            if m in current:
+                option = input('옵션을 선택하세요: ')
+                # current 값 변경
+                current = current[option]
+    return f"주문한 메뉴: {menu_name}, 가격: {current * quantity}원"
 
-    # 딕셔너리일 때 : Cafe Latte X
-    if type(menu[menu_name]) == dict:
-        # option이 필요함 menu[menu_name][option] << 뭐가 들어가냐가 필요한데 Ice/Hot or 다른 메뉴 
-        option = input()
-        # option이 Ice or Hot일 때
-        temperature = ['Ice', 'Hot']
-
-        if option in temperature:
-            return f"주문한 메뉴 - {menu_name} ({option}), 가격: {menu[menu_name][option] * quantity}원"
-        
-        # option이 Ice or Hot이 아니고,
-        if option not in temperature:
-
-            # 딕셔너리가 아닐 때 : Earl Grey, Green Tea, Chamomile X
-            if type(menu[menu_name][option]) != dict:
-                return f"주문한 메뉴 - {menu_name}: {option}, 가격: {menu[menu_name][option] * quantity}원"
-
-            # 딕셔너리일 때 : Earl Grey, Green Tea, Chamomile
-            if type(menu[menu_name][option]) == dict:
-                # Ice or hot을 선택해야함
-                option_temp = input()
-                return f"주문한 메뉴 - {menu_name}: {option} ({option_temp}), 가격: {menu[menu_name][option][option_temp] * quantity}원"
-        
 print(order_menu(choice, quantity))
 
-
-
-# TODO 수정할 부분 : 딕셔너리 내의 값들을 구분할 때 너무 딱딱하여 유지보수, 수정이 힘듦
-# if isinstance(menu[menu_name], int) -> menu[menu_name]을 했을 때 int가 나오냐, 나오지 않냐 
-# 나오지 않으면 다른것이 더 필요하다는 뜻.. while문 사용
+# TODO 반복문 안쪽 예외처리하기, 가장 마지막 return값 수정
