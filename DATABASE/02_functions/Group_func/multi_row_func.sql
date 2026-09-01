ae702b5056437e249a7d76aec71db40432b10096
kanghanbin <hanbin0723@g.yju.ac.kr>
2026-09-01 17:47:01 +0900

    폴더 추가

---
+++ DATABASE/02_functions/Group_func/multi_row_func.sql
@@ -0,0 +1,57 @@
+-- SUM
+SELECT SUM(SAL)
+FROM EMP;
+
+SELECT SUM(SAL),
+       SUM(ALL SAL), -- SUM(SAL)과 동일한 결과
+       SUM(DISTINCT SAL) -- 동일한 급여는 제거 후 계산
+FROM EMP;
+
+SELECT SUM(SAL), SUM(COMM)
+FROM EMP;
+
+-- COUNT
+SELECT COUNT(*) -- EMP 테이블의 데이터 개수 구하기
+FROM EMP;
+
+SELECT COUNT(*) AS "부서 번호가 30번인 직원"
+FROM EMP
+WHERE DEPTNO = 30;
+
+SELECT COUNT(SAL),
+       COUNT(DISTINCT SAL) -- SAL중 중복을 제거하고 COUNT
+FROM EMP;
+
+SELECT COUNT(*) AS "추가수당 열 구하기 NULL 사용"
+FROM EMP
+WHERE COMM IS NOT NULL;
+ 
+SELECT COUNT(COMM) 
+FROM EMP;
+
+-- MAX, MIN
+SELECT MAX(SAL) AS "부서 번호가 10번인 직원에서 가장 높은 급여"
+FROM EMP
+WHERE DEPTNO = 10;
+
+SELECT MIN(SAL) AS "부서 번호가 10번인 직원에서 가장 낮은 급여"
+FROM EMP
+WHERE DEPTNO = 10;
+
+-- 부서번호가 20인 사원의 입사일중 가장 최근 입사일 구하기 : 2021-01-01 < 2022-01-01
+SELECT TO_CHAR(MAX(HIREDATE), 'YYYY/MM/DD')
+FROM EMP
+WHERE DEPTNO = 20;
+-- 부서번호가 20인 사원의 입사일중 가장 오래된 입사일 구하기
+SELECT TO_CHAR(MIN(HIREDATE), 'YYYY/MM/DD')
+FROM EMP
+WHERE DEPTNO = 20;
+
+-- AVG
+SELECT AVG(SAL) AS "부서 번호가 30인 사원들의 평균 급여"
+FROM EMP
+WHERE DEPTNO = 30;
+
+SELECT AVG(DISTINCT(SAL)) AS "부서 번호가 30인 사원들의 중복을 제거한 평균 급여"
+FROM EMP
+WHERE DEPTNO = 30;
