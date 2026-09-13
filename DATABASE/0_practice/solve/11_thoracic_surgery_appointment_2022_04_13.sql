a19fa06dcf76bc0b34bbce011f1431469bf6b557
kanghanbin <hanbin0723@g.yju.ac.kr>
2026-09-13 13:47:51 +0900

    docs: 11번 2022년 4월 13일 흉부외과 진료 예약 조회

---
+++ DATABASE/0_practice/solve/11_thoracic_surgery_appointment_2022_04_13.sql
@@ -0,0 +1,16 @@
+-- 2022-04-13일 취소되지 않은 흉부외과 진료 예약 내역 조회하는 SQL
+-- 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시
+-- 진료예약일시 기준 오름차순
+
+-- 3개의 테이블을 JOIN해야함
+-- PATIENT의 PT_NO = APPOINTMENT의 PT_NO
+-- DOCTOR DR_ID = APPOINTMENT MDDR_ID
+SELECT A.APNT_NO, P.PT_NAME, A.PT_NO, A.MCDP_CD, D.DR_NAME, A.APNT_YMD
+FROM PATIENT P JOIN APPOINTMENT A
+ON P.PT_NO = A.PT_NO
+JOIN DOCTOR D ON D.DR_ID = A.MDDR_ID
+WHERE A.APNT_CNCL_YN = 'N'
+AND TO_CHAR(A.APNT_YMD, 'YYYY-MM-DD') = '2022-04-13'
+AND A.MCDP_CD = 'CS'
+ORDER BY A.APNT_YMD ASC;
+-- DCOTOR테이블과 APPOINTMENT테이블을 JOIN할 때 MCDP_CD으로 하면 여러 값이 나올 수 있음을 주의
