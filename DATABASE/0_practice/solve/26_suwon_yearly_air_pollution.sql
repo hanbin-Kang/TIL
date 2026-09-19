a8508075bdc2c8d45f90b48094f67169ccff8be6
kanghanbin <hanbin0723@g.yju.ac.kr>
2026-09-20 07:42:17 +0900

    docs: 26번 수원지역 연도별 평균 미세먼지 오염도 조회

---
+++ DATABASE/0_practice/solve/26_suwon_yearly_air_pollution.sql
@@ -0,0 +1,8 @@
+-- 수원지역의 연도별 평균 미세먼지 오염도, 평균 초미세먼지 오염도를 조회하는 SQL
+SELECT EXTRACT(YEAR FROM YM) AS YEAR,
+       ROUND(AVG(PM_VAL1), 2) AS "PM10",
+       ROUND(AVG(PM_VAL2), 2) AS "PM2.5"
+FROM AIR_POLLUTION
+WHERE LOCATION2 = '수원'
+GROUP BY YEAR
+ORDER BY YEAR ASC;
